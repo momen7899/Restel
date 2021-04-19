50e550515cb1d0b0d97c65b5372c716d93a4913a
Momen Hamaveysi <momen.hamaveysi@kubakgroup.com>
2021-04-19 10:35:48 +0430

    Do Some Changes

--- presentation/src/main/java/com/momen/restel/main/view/ReserveAdapter.kt
+++ presentation/src/main/java/com/momen/restel/main/view/ReserveAdapter.kt
@@ -1,37 +1,31 @@
 package com.momen.restel.main.view
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.momen.restel.R
+import com.momen.restel.databinding.ReservationItemBinding
 import com.momen.restel.reserve.model.ReserveModel
 
 class ReserveAdapter : RecyclerView.Adapter<ReserveAdapter.ReserveViewHolder>() {
 
     private val items = ArrayList<ReserveModel>()
 
-    class ReserveViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        val customer: TextView = view.findViewById(R.id.reservationItemCustomer)
-        val client: TextView = view.findViewById(R.id.reservationItemClient)
-        val room: TextView = view.findViewById(R.id.reservationItemRoomName)
-        val price: TextView = view.findViewById(R.id.reservationItemPrice)
-    }
+    class ReserveViewHolder(val reserve: ReservationItemBinding) :
+        RecyclerView.ViewHolder(reserve.root)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ReserveViewHolder =
         ReserveViewHolder(
-            LayoutInflater.from(parent.context)
-                .inflate(R.layout.reservation_item, parent, false)
+            DataBindingUtil.inflate(
+                LayoutInflater.from(parent.context),
+                R.layout.reservation_item, parent, false
+            ) as ReservationItemBinding
         )
 
     override fun onBindViewHolder(holder: ReserveViewHolder, position: Int) {
-        with(holder) {
-            customer.text = items[position].customer
-            client.text = items[position].client
-            room.text = items[position].room
-            price.text = items[position].price.toString().plus(" تومان")
-        }
+        val currentReserve: ReserveModel = items[position]
+        holder.reserve.reserve = currentReserve
     }
 
     override fun getItemCount(): Int = items.size
