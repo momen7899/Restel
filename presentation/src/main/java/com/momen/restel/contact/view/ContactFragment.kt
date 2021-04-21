0ff8a2f528542ab182e13e906d1e060fd29e5e6e
Momen Hamaveysi <momen.hamaveysi@kubakgroup.com>
2021-04-21 09:01:13 +0430

    Init Contact Page

---
+++ presentation/src/main/java/com/momen/restel/contact/view/ContactFragment.kt
@@ -0,0 +1,23 @@
+package com.momen.restel.contact.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.momen.restel.R
+
+class ContactFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? = inflater.inflate(R.layout.fragment_contact, container, false)
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+//        setUpComponents()
+    }
+}
