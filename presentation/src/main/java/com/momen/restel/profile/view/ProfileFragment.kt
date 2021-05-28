36092cb7e4c599be8db4c19588c6c79e6faea075
Momen Hamaveysi <momen.hamaveysi@kubakgroup.com>
2021-05-28 16:44:37 +0430

    Init Profile and Settings page

--- presentation/src/main/java/com/momen/restel/profile/view/ProfileFragment.kt
+++ presentation/src/main/java/com/momen/restel/profile/view/ProfileFragment.kt
@@ -4,13 +4,61 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
 import com.momen.restel.R
+import kotlinx.android.synthetic.main.fragment_contact.*
+import kotlinx.android.synthetic.main.fragment_profile.*
 
 class ProfileFragment : Fragment() {
+    private val rtl = true
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? = inflater.inflate(R.layout.fragment_profile, container, false)
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        setUpComponents()
+    }
+
+    private fun setUpComponents() {
+        hideActivityComponent()
+        setDirection()
+        setBack()
+        setViews()
+    }
+
+    private fun hideActivityComponent() {
+        hideNavMenu()
+        hideToolbar()
+    }
+
+    private fun hideNavMenu() {
+        requireActivity().findViewById<DrawerLayout>(R.id.mainDrawer)
+            .setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED)
+    }
+
+    private fun hideToolbar() {
+        requireActivity().findViewById<Toolbar>(R.id.toolbar).visibility = View.GONE
+    }
+
+
+    private fun setDirection() {
+        profileFragment.layoutDirection =
+            if (rtl) View.LAYOUT_DIRECTION_RTL else View.LAYOUT_DIRECTION_LTR
+    }
+
+    private fun setBack() {
+        profileBack.setOnClickListener {
+            requireActivity().onBackPressed()
+        }
+    }
+
+    private fun setViews() {
+    }
+
 }
